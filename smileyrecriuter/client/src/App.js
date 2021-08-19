6ba0b5bf5bd758c9b64461594df792742c5f77a9
Nazala <nazala5151@gmail.com>
2021-08-19 13:39:41 -0500

    page is onlinegit add .

--- smileyrecriuter/client/src/App.js
+++ smileyrecriuter/client/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Body from './components/body';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Body />
+      <Footer />
+    </>
+    );
 }
 
 export default App;
+
+
+
+
+
+
+
+// Mern > Act 19 > client > src > App.js
+
+// import Home from './pages/Home';
+// import Profile from './pages/Profile';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
+
+// const client = new ApolloClient({
+//   uri: '/graphql',
+//   cache: new InMemoryCache(),
+// });
+
+// function App() {
+//   return (
+//     <ApolloProvider client={client}>
+//       {/* Wrap page elements in Router component to keep track of location state */}
+//       <Router>
+//         <div className="flex-column justify-flex-start min-100-vh">
+//           <Header />
+//           <div className="container">
+//             {/* Define routes to render different page components at different paths */}
+//             <Route exact path="/">
+//               <Home />
+//             </Route>
+//             {/* Define a route that will take in variable data */}
+//             <Route exact path="/profiles/:profileId">
+//               <Profile />
+//             </Route>
+//           </div>
+//           <Footer />
+//         </div>
+//       </Router>
+//     </ApolloProvider>
+//   );
+// }
+
+// export default App;
+
---
+++ smileyrecriuter/client/src/components/HeaderNav/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+// import './style.css';
+
+export default function HeaderNav ({ currentPage, handlePageChange }) {
+    return (
+        <header className="header" > 
+            <h1 className="name-header">Smiley Recruiter</h1>
+
+            <nav>
+                <ul className="nav nav-list justify-content-end">
+
+                    <li className="nav-item">
+                        <a href="#searchPage"
+                            onClick={() => handlePageChange('SearchPage')}
+                            className={currentPage === 'SearchPage' ? 'nav-link disabled' : 'nav-link'}>
+                            SearchPage
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#profilePage"
+                            onClick={() => handlePageChange('ProfilePage')}
+                            className={currentPage === 'ProfilePage' ? 'nav-link disabled' : 'nav-link'}>
+                            ProfilePage
+                        </a>
+                    </li>
+
+                    <li className="nav-item">
+                        <a href="#loginPage"
+                            onClick={() => handlePageChange('LoginPage')}
+                            className={currentPage === 'LoginPage' ? 'nav-link disabled' : 'nav-link'}>
+                            LoginPage
+                        </a>
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+
+
+
