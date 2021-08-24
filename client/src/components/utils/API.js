7a082beae6114d0b55dd64a6afb649a157360523
Nazala <nazala5151@gmail.com>
2021-08-23 22:50:43 -0500

    heroku is stupid

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -2,7 +2,7 @@ import env from "react-dotenv";
 
 
 export const searchJobs = async (title, location) => {
-  return await fetch(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${env.APP_ID}&app_key=${env.APP_KEY}&results_per_page=100&what=${title}&location0=us&location1=${location}`);
+  return await fetch(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=0a7ada16&app_key=d5fa615b8c01ac683eaceb5645376f7e&results_per_page=100&what=${title}&location0=us&location1=${location}`);
 };
   
 export const getTopCompanies = async (title, location) => {
