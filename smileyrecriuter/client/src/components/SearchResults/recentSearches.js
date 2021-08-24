9556cd56cf66cf15077d31588b533d0c98033798
mray1995 <sleepingbag5927@gmail.com>
2021-08-23 21:21:12 -0500

    fixed

--- smileyrecriuter/client/src/components/SearchResults/recentSearches.js
+++ smileyrecriuter/client/src/components/SearchResults/recentSearches.js
@@ -5,63 +5,55 @@ export default function RecentSearches(props) {
 
 
     const [recentSearches, setRecentSearches] = useState([])
-    const [renderReady, setRenderReady] = useState("false")
-
-   
-
 
     const renderRecentSearches = () => {
         let list;
-        if(recentSearches){
-            list = recentSearches.map((search, i)=> {
+        if (recentSearches) {
+            list = recentSearches.map((search, i) => {
                 return (
-                    <li className="list-group-item"key={i}><button onClick={()=>props.setSearchObj({jobTitle: search.jobTitle, location: search.location})}> {search.jobTitle} in {search.location} </button>{search.jobTitle} in {search.location}</li>
+                    <li className="list-group-item" key={i}>
+                        <button onClick={() => props.setSearchObj({ jobTitle: search.jobTitle, location: search.location })}>
+                            {search.jobTitle} in {search.location}
+                        </button>
+                    </li>
                 )
             })
-           
+
         }
         return list
     }
 
-
-
-
     useEffect(() => {
         if (recentSearches.length < 5 && props.searchObj.jobTitle) {
-            setRecentSearches([ props.searchObj, ...recentSearches])
-        } 
-         else if(recentSearches.length > 4 ){
-             console.log("over the limit");
-             let copy = recentSearches.slice()
-             let newCopy = copy.pop()
-             setRecentSearches([ props.searchObj, ...copy])
-         }
+            setRecentSearches([props.searchObj, ...recentSearches])
+        }
+        else if (recentSearches.length > 4) {
+            console.log("over the limit");
+            let copy = recentSearches.slice()
+            let newCopy = copy.pop()
+            setRecentSearches([props.searchObj, ...copy])
+        }
     }, [props.searchObj.jobTitle, props.searchObj.location]);
 
-    
 
-    
+    if (recentSearches.length) {
+        return (
+            <>
+                {recentSearches.length && (
+
+                    <div className="border border-dark p-2 m-2">
+                        <h5 className="card-title cardHeaders">Recent Searches</h5>
+                        <ul className="list-group">
+                            {renderRecentSearches()}
+
+                        </ul>
+                    </div>
+                )}
 
 
-    
-    if(recentSearches.length){
-    return (
-        <>
-        {recentSearches.length && (
-            
-                <div className="border border-dark p-2 m-2">
-                <h5 className="card-title cardHeaders">Recent Searches</h5>
-                <ul className="list-group">
-                    {renderRecentSearches()}
-                    
-                </ul>
-            </div>
-            )}
-        
-    
-    </>
-    )
-        }else{return null}
+            </>
+        )
+    } else { return null }
 }
 
 
